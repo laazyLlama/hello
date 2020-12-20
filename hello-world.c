b0afb6720f8b13a0cf0a4c23229e62d4315dce20
laazyLlama <laazy.llama@gmail.com>
2020-12-20 11:55:31 +0100

    added header comment

--- hello-world.c
+++ hello-world.c
@@ -1,3 +1,13 @@
+//-----------------------------------------------------------------------------
+//Project Name:     hello/hello-world.c
+//Description:      A small example project, to try out git on macOS using
+//                  commandline and atom built in tools.
+//-----------------------------------------------------------------------------
+//Author:           laazyLlama
+//Date Created:     20/12/2020
+//Last Changes:     20/12/2020
+//-----------------------------------------------------------------------------
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
