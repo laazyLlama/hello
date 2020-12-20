4dcda2c342027eb429dc41123e53b969d81b4cb6
laazyLlama <laazy.llama@gmail.com>
2020-12-20 11:54:17 +0100

    using commandline arguments

--- hello-world.c
+++ hello-world.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-  printf("Hello World!\n");
-  printf("This is my first commandline created git repo");
+int main(int argc, char *argv[]) {
+  if(argc == 2)
+    printf("Hello %s!\n", argv[1]);
+  else if(argc > 2)
+    printf("Too many arguments supplied!\n");
+  else
+    printf("Hello World!\n");
+  printf("This is my first commandline created git repo.\n");
   return 0;
 }
